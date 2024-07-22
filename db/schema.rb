2b85f9302aaf12710a08a543070590534bc5759f
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-07-22 09:29:43 -0600

    Add/Update model tests - passing

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_09_08_172657) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_22_151613) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customer_items", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_customer_items_on_customer_id"
+    t.index ["item_id"], name: "index_customer_items_on_item_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -30,5 +45,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_09_08_172657) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "customer_items", "customers"
+  add_foreign_key "customer_items", "items"
   add_foreign_key "items", "supermarkets"
 end
