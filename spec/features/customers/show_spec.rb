8b64146489a8d689c8eea73a4387943f83cc69c7
Austin K <austink@Austins-MacBook-Pro.local>
2024-07-22 11:59:03 -0400

    Added show customer test

---
+++ spec/features/customers/show_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe "Customer Show page" do
+  it "shows the customer name and their items" do
+    supermarket = Supermarket.create!(name: "Corner Market", location: "123 Main St")
+    customer = Customer.create!(name: "Sally")
+    item1 = Item.create!(name: "Banana", price: 1, supermarket: supermarket)
+    item2 = Item.create!(name: "Apple", price: 2, supermarket: supermarket)
+    customer.items << [item1, item2]
+
+    visit "/customers/#{customer.id}"
+
+    expect(page).to have_content("Sally")
+    expect(page).to have_content("Banana")
+    expect(page).to have_content("$1")
+    expect(page).to have_content("Corner Market")
+    expect(page).to have_content("Apple")
+    expect(page).to have_content("$2")
+    expect(page).to have_content("Corner Market")
+  end
+end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
-RSpec.describe Customer
+RSpec.describe Customer, type: :model do
   describe "relationships" do
     it {should have_and_belong_to_many :items}
   end
+end
