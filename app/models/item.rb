ea933f2400cad5c08d41e98716bc810531f4b775
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-22 09:30:50 -0600

    Add fix for typo in item.rb model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :supermarket
   has_many :customer_items
-  has _many :customers, through: :customer_items
+  has_many :customers, through: :customer_items
 end
