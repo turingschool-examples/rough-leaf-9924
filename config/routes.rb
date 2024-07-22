6b90461dd995345199e9b61a57f59269d55ac165
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-22 11:38:57 -0600

    Add route to see where it would lead me. Unescessary

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
+  # get "/customers", to: "customers#index"    
   get "/customers/:id", to: "customers#show"
   post "/customers/:id/items", to: "customer_items#create"
 end
