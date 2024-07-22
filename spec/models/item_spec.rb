1c73d0dde1b499d539aa41f7d464e8eb7abc30c7
Jan Lehigh <janlehigh461437@gmail.com>
2024-07-22 09:28:17 -0600

    Add model test for relationships in the item_spec and customer_spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Item, type: :model do
   describe 'relationships' do
     it { should belong_to :supermarket }
     it { should have_many :customer_items }
-    it { should have_many :customers.through(:customer_items) }
+    it { should have_many(:customers).through(:customer_items) }
   end
 end
